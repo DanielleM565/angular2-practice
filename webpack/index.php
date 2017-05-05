5827b917340cf44091b1f3b740572ca255d0fc8f
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-05 00:32:10 -0600

    added template for entire application

---
+++ webpack/index.php
@@ -0,0 +1,19 @@
+<?php
+if(session_status() !== PHP_SESSION_ACTIVE){
+	session_start();
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
+
+		<title>Angular 2 Practice</title>
+	</head>
+	<body>
+		<angular2-practice-app>Loading&hellip;</angular2-practice-app>
+	</body>
+</html>
