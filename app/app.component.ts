cdd617439698d3756889a5d60eb10c41eddb399d
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-07 13:35:53 -0600

    made 'quotes' to "quotes" in app.components.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,10 +2,9 @@ import {Component} from "@angular/core";
 
 @Component({
 	//Update selector with YOU__APP_NAME-app. This needs to match the custom tag in webpack/index.php
-	selector: 'angular2-practice-app',
+	selector: "angular2-practice-app",
 
 	//templateURL path to your public_html/templates directory.
-	templateUrl: './templates/angular2-practice-app.php'
-})
+	templateUrl: "./templates/angular2-practice-app.php" })
 
 export class AppComponent {}
